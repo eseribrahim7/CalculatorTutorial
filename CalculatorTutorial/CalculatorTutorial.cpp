907a39cd4812df9791aa14703464fb9ce20c39e3
İbrahim ESER <eseribrahim7@outlook.com>
2024-02-23 12:45:41 +0300

    Added  debug code for dividing by 0 error

--- CalculatorTutorial/CalculatorTutorial.cpp
+++ CalculatorTutorial/CalculatorTutorial.cpp
@@ -17,14 +17,21 @@ int main()
     char oper = '+';
 
     cout << "Calculator Console Application" << endl << endl;
-    cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b"
-        << endl;
+    cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b" << endl;
 
     Calculator c;
     while (true)
     {
         cin >> x >> oper >> y;
-        result = c.Calculate(x, oper, y);
+        if (oper == '/' && y == 0)
+        {
+            cout << "Attempted to divide by zero!" << endl;
+            continue;
+        }
+        else
+        {
+            result = c.Calculate(x, oper, y);
+        }
         cout << "Result " << "of " << x << oper << y << " is: " << result << endl;
     }
 
